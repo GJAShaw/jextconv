ea5600f43ccffe8da1448868af88a103e19b37cd
Greg Shaw <g.j.a.shaw@gmail.com>
2020-01-04 19:11:39 +0000

    Make first commit - skeleton rep

---
+++ src/main/java/com/avalokita/jextconv/App.java
@@ -0,0 +1,13 @@
+package com.avalokita.jextconv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/avalokita/jextconv/DldConv.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ */
+package com.avalokita.jextconv;
+
+/**
+ * @author greg
+ *
+ */
+public class DldConv {
+
+	/**
+	 * @param args one filename
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Suck my jextconv balls! " + Math.random());
+
+	}
+
+}
