262b8aa4169af1df287fd0754f44aea12f4b055f
Greg Shaw <g.j.a.shaw@gmail.com>
2020-01-05 13:29:30 +0000

    Use Log4j2 instead of System.out

--- src/main/java/com/avalokita/jextconv/DldConv.java
+++ src/main/java/com/avalokita/jextconv/DldConv.java
@@ -7,14 +7,19 @@ package com.avalokita.jextconv;
  * @author greg
  *
  */
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class DldConv {
+	
+	private static final Logger LOG = LogManager.getLogger(DldConv.class);
 
 	/**
 	 * @param args one filename
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println("Suck my jextconv balls! " + Math.random());
+		LOG.info("Suck my jextconv balls! " + Math.random());
 
 	}
 
