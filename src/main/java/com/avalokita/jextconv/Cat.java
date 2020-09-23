63fffc694ded40709e7c3aac795e84b8de90c189
Greg Shaw <g.j.a.shaw@gmail.com>
2020-09-23 20:43:44 +0100

    Define business class Cat

---
+++ src/main/java/com/avalokita/jextconv/Cat.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.avalokita.jextconv;
+
+/**
+ * @author greg
+ *
+ */
+public class Cat {
+
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	private String name;
+	
+	private int age;
+}
